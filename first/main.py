93870ee3057022a7fa94225353c04a9a218e285f
Andrey Sokolov <falconandy@yandex.ru>
2018-08-21 13:57:13 +0400

    initial commit

---
+++ first/main.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    return x + y
+
+
+def mult(x, y):
+    return x * y
+
---
+++ tests/test_first.py
@@ -0,0 +1,11 @@
+import unittest
+
+from first.main import add, mult
+
+
+class FirstTests(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(5, add(2, 3))
+
+    def test_mult(self):
+        self.assertEqual(6, mult(2, 3))
